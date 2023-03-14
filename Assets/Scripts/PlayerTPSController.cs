bb15c44f02ccf6bdcb4629b1f6c7b0d3f2b62701
MatiuxG <mateogal2409@gmail.com>
2023-03-14 14:26:55 +0100

    Escenario

---
+++ Assets/Scripts/PlayerTPSController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTPSController : MonoBehaviour
+{
+    public Camera cam;
+    private InputData input;
+    private CharacterAnimBasedMovement characterMovement;
+
+    void Start()
+    {
+        characterMovement = GetComponent<CharacterAnimBasedMovement>();
+    }
+
+    void Update()
+    {
+        // Get input from player
+        input.getInput();
+
+        // Apply input to character
+        characterMovement.moveCharacter(input.hMovement, input.vMovement, cam, input.jump, input.dash);
+    }
+}
